a47861f22a97c982e675acdf9ef2585c64dfa52a
cc home <cc258520@gmail.com>
2019-07-29 23:56:26 +0800

    feat: xml & map

--- web/static/src/pages/simulator/map.jsx
+++ web/static/src/pages/simulator/map.jsx
@@ -1,44 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
-import { divIcon } from "leaflet";
 
-export default function SimulatorMap() {
-  const [position, setPosition] = useState([51.505, -0.09]);
-  const [zoom, setZoom] = useState(18);
-  const [deg, setDeg] = useState(95);
-  const [icon, setIcon] = useState();
-
-  function handIcon() {
-    const icon = divIcon({
-      iconAnchor: [20, 30],
-      iconSize: [40, 60],
-      className: "icon-car",
-      html: `<span style='transform:rotate(${deg}deg)'></span>`
-    });
-
-    setIcon(<Marker position={position} icon={icon} />);
-  }
-
-  useEffect(() => {
-    handIcon();
-  }, [deg]);
-
+export default function SimulatorMap({ position }) {
+  console.log(position);
   return (
     <LeafletMap
       center={position}
       fadeAnimation={false}
-      zoom={zoom}
+      zoom={18}
       zoomControl={false}
-      minZoom={13}
-      maxZoom={20}
     >
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        minZoom={8}
-        maxZoom={13}
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {icon}
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
     </LeafletMap>
   );
 }
--- web/static/src/pages/simulator/simulator.jsx
+++ web/static/src/pages/simulator/simulator.jsx
@@ -1,23 +1,54 @@
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { useState, useEffect } from "react";
+const convert = require("xml-js");
+const xml =
+  '<?xml version="1.0" encoding="utf-8"?><note importance="high" logged="true"><trkpt lat="51.377719" lon="12.338217"><time>1970-01-01T00:00:00+00:00</time></trkpt><trkpt lat="51.37703" lon="12.339166"><time>1970-01-01T00:00:14+00:00</time></trkpt><trkpt lat="51.37754" lon="12.340111"><time>1970-01-01T00:00:35+00:00</time></trkpt><trkpt lat="51.376201" lon="12.341625"><time>1970-01-01T00:01:03+00:00</time></trkpt><trkpt lat="51.376151" lon="12.34196"><time>1970-01-01T00:01:04+00:00</time></trkpt></note>';
+var result1 = convert.xml2json(xml, { compact: true, spaces: 2 });
+
+const result = JSON.parse(result1).note.trkpt;
+// {
+//         "_attributes": {
+//           "lat": "51.377719",
+//           "lon": "12.338217"
+//         },
+//         "time": {
+//           "_text": "1970-01-01T00:00:00+00:00"
+//         }
+//       }
+const trkpt = result.map(item => {
+  return {
+    point: {
+      lat: item._attributes.lat,
+      lon: item._attributes.lon
+    },
+    text: item.time._text
+  };
+});
+
+console.log(trkpt);
+
 import SimulatorMap from "./map.jsx";
 
 export default function Simulator() {
+  const [random, setRandom] = useState();
   const [count, setCount] = useState(0);
-  const [msg, setMsg] = useState([123]);
+  const [position, setPosition] = useState(0, 0);
+  const [msg, setMsg] = useState([0]);
   const [actionClass, setActionClass] = useState("");
 
   useEffect(() => {
     const start = setInterval(() => {
-      setCount(Math.random());
+      setRandom(Math.random());
     }, 1000);
     return () => {
       clearInterval(start);
     };
   }, []);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
+    setCount(count + 1);
     creatMsg();
-  }, [count]);
+    setPosition(trkpt[count % 4].point);
+  }, [random]);
 
   function creatMsg() {
     setActionClass("action");
@@ -25,7 +56,7 @@ export default function Simulator() {
       setActionClass("");
     }, 400);
     console.log("renderMsg f", msg[0], actionClass);
-    const msgText = [msg[0] + 1];
+    const msgText = [msg[0] + 10];
     setMsg(msgText.concat(msg));
   }
 
@@ -47,7 +78,7 @@ export default function Simulator() {
         </div>
         <div className="simulator-card">
           1234567
-          <SimulatorMap />
+          <SimulatorMap position={position} />
         </div>
         <div className="simulator-card">
           <ul className="simulator-log">{renderMsg()}</ul>
