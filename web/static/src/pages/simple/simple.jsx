2e0bb9f5b6ccf79b7d7a0aad4355c3a7ac8fdcc4
Leo <chuang.cui@ericsson.com>
2019-08-01 15:08:43 +0800

    merge

--- web/static/src/pages/simple/simple.jsx
+++ web/static/src/pages/simple/simple.jsx
@@ -1,18 +1,6 @@
-<<<<<<< Updated upstream
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-// import { testPoint } from '../../components/test.js';
-import { TRIP_ONE } from '../../components/trip_path_20190713154335.js';
-import { TRIP_TWO } from '../../components/trip_path_20190714180533.js';
-
-import Map from '../../components/map.jsx';
-import { decode } from '../../utils/decode';
-=======
 import React, { useState, useEffect } from "react";
 
 import Map from "../../components/map.jsx";
->>>>>>> Stashed changes
 
 export default function Simulator() {
   const [centerPoint, setCenterPoint] = useState([0, 0]);
@@ -69,11 +57,11 @@ export default function Simulator() {
 
   function getServersPoint() {
     axios
-      .post('http://100.98.137.10:8879/v1/pos-match/123?type=poi', TRIP_TWO)
+      .post("http://100.98.137.10:8879/v1/pos-match/123?type=poi", TRIP_TWO)
       .then(res => {
         res.data && res.data.paths && res.data.paths.length > 0
           ? setOptimizedTrip(decode(res.data.paths[0].points, false))
-          : console.warn('get path problem');
+          : console.warn("get path problem");
       });
   }
 
